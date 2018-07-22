4324a3d932360f81a599f2cef2fde7a01ad35ddb
siuols <louisaleksieb.dagusen@gmail.com>
2018-07-22 18:43:53 +0800

    Create a class for Page detail Update urls.py for slug

--- project/urls.py
+++ project/urls.py
@@ -17,12 +17,13 @@ from django.conf.urls import url
 
 from django.contrib import admin
 
-from pages.views import HomeView
+from pages.views import HomeView, PageDetailView
 
 from newsletter.api.views import JoinAPICreateView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^(?P<slug>[\w-]+)$', PageDetailView.as_view(), name='page-detail'),
     url(r'^admin/', admin.site.urls),
     url(r'^api/email/join/$', JoinAPICreateView.as_view(), name='email-join'),
 ]
